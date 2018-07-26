cd8dd66efb18226ed919ce0a23bad5921c34dfef
alana-m-552 <alana.mackey0552@gmail.com>
2018-07-26 20:13:49 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,29 @@
 # book.rb
 # Add you Book class here
+class Book
+  def initialize(title)
+    return @title = title 
+  end 
+  
+  def title 
+    @title 
+  end 
+  
+  def author=(author)
+    return @author = author
+  end
+  
+  def author
+    @author
+  end
+  
+  def page_count
+    @page_count
+  end 
+  
+  def genre
+    @genre
+  end 
+end 
+Book.new("And Then There Were None")
+ 
